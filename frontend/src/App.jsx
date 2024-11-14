a78b439334c38162145b9859258856e8433ca93f
Arpit Gautam <arpitgautam73@gmail.com>
2024-11-14 12:18:22 +0530

    navbar skeleton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,9 +8,11 @@ import Contact from './pages/Contact'
 import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
 const App = () => {
   return (
-    <h1 className="mx-4 sm:mx-[10%]">
+    <div className="mx-4 sm:mx-[10%]">
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/doctors' element={<Doctors />} />
@@ -22,10 +24,10 @@ const App = () => {
         <Route path='/my-appointments' element={<MyAppointments />} />
         <Route path='/appointment/:docId' element={<Appointment />} />
       </Routes>
-    </h1>
+    </div>
   )
 }
 
 export default App
 
-//28:54
+//42:23
