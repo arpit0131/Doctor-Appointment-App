3bc0b323d596ec8bff3699f36582c1871f0d4a99
Arpit Gautam <arpitgautam73@gmail.com>
2024-12-28 23:58:17 +0530

    footer css fixed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { assets } from '../assets/assets_frontend/assets';
 const Footer = () => {
   return (
-    <div className='md:mx-10'>
+    <div>
       <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
         {/**-------Left--------- */}
         <div>
@@ -26,7 +26,7 @@ const Footer = () => {
           </ul>
         </div>
         {/**-------Right--------- */}
-        <div>
+        <div className='md:text-end'>
           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
             <li>+91 9867489276</li>
