fc5853fc17578ae8c29faa85fe05cf8aed3f206a
Arpit Gautam <arpitgautam73@gmail.com>
2024-12-29 01:25:24 +0530

    navbar responsive ness fixed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,87 +8,93 @@ const Navbar = () => {
   const [token, setToken] = useState(true);
 
   return (
-    <div className='flex items-center justify-between text-sm py-4 pl-44 pr-44 mb-5 border-b border-b-gray-400 fixed top-0 left-0 w-full bg-white z-10 shadow-md'>
-      <img
-        onClick={() => {
-          navigate('/');
-        }}
-        className='w-44 cursor-pointer'
-        src={assets.logo}
-        alt=''
-      />
-      <ul className='hidden lg:flex items-start gap-16 font-medium text-lg'>
-        <NavLink to='/'>
-          <li className='py-1'>HOME</li>
-          <hr className='border-none outline-none h-1 bg-primary w-5/5 m-auto hidden' />
-        </NavLink>
-        <NavLink to='/doctors'>
-          <li className='py-1'>ALL DOCTORS</li>
-          <hr className='border-none outline-none h-1 bg-primary w-5/5 m-auto hidden' />
-        </NavLink>
-        <NavLink to='/about'>
-          <li className='py-1'>ABOUT</li>
-          <hr className='border-none outline-none h-1 bg-primary w-5/5 m-auto hidden' />
-        </NavLink>
-        <NavLink to='/contact'>
-          <li className='py-1'>CONTACT</li>
-          <hr className='border-none outline-none h-1 bg-primary w-5/5 m-auto hidden' />
-        </NavLink>
-      </ul>
-      <div className='flex items-center gap-4'>
-        {token ? (
-          <div className='flex items-center gap-2 cursor-pointer group relative'>
-            <img
-              className='w-8 rounded-full'
-              src={assets.profile_pic}
-              alt='Profile pic'
-            />
-            <img
-              className='w-2.5'
-              src={assets.dropdown_icon}
-              alt='Dropdown icon'
-            />
-            <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
-              <div className='min-w-48 bg-stone-100 rounded-lg flex flex-col'>
-                <p
-                  onClick={() => navigate('/my-profile')}
-                  className='p-3 hover:text-black cursor-pointer hover:bg-stone-300 hover:rounded-t-lg'
-                >
-                  My Profile
-                </p>
-                <p
-                  onClick={() => navigate('/my-appointments')}
-                  className='p-3 hover:text-black cursor-pointer hover:bg-stone-300 '
-                >
-                  My Appointments
-                </p>
-                <p
-                  onClick={() => setToken(false)}
-                  className='p-3 hover:text-black cursor-pointer hover:bg-stone-300 hover:hover:rounded-b-lg'
-                >
-                  Logout
-                </p>
+    <div className='fixed top-0 left-0 bg-white w-full z-10 px-5 py-2.5 border-b border-gray-400'>
+      {/* Desktop View */}
+      <div className='flex justify-between'>
+        <img
+          onClick={() => {
+            navigate('/');
+          }}
+          className='w-44 cursor-pointer'
+          src={assets.logo}
+          alt=''
+        />
+        <ul className='hidden lg:flex items-center gap-8 text-lg font-medium'>
+          <NavLink to='/'>
+            <li className='py-1'>HOME</li>
+            <hr className='h-1 bg-primary w-5/5 hidden' />
+          </NavLink>
+          <NavLink to='/doctors'>
+            <li className='py-1'>ALL DOCTORS</li>
+            <hr className='h-1 bg-primary w-5/5 hidden' />
+          </NavLink>
+          <NavLink to='/about'>
+            <li className='py-1'>ABOUT</li>
+            <hr className='h-1 bg-primary w-5/5 hidden' />
+          </NavLink>
+          <NavLink to='/contact'>
+            <li className='py-1'>CONTACT</li>
+            <hr className='h-1 bg-primary w-5/5 hidden' />
+          </NavLink>
+        </ul>
+        <div className='flex gap-4'>
+          {token ? (
+            <div className='flex items-center gap-2 cursor-pointer group relative'>
+              <img
+                className='w-12 rounded-full'
+                src={assets.profile_pic}
+                alt='Profile pic'
+              />
+              <img
+                className='w-3'
+                src={assets.dropdown_icon}
+                alt='Dropdown icon'
+              />
+              <div className='absolute top-2 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+                <div className='min-w-48 bg-stone-100 rounded-lg flex flex-col'>
+                  <p
+                    onClick={() => navigate('/my-profile')}
+                    className='p-3 hover:text-black cursor-pointer hover:bg-stone-300 hover:rounded-t-lg'
+                  >
+                    My Profile
+                  </p>
+                  <p
+                    onClick={() => navigate('/my-appointments')}
+                    className='p-3 hover:text-black cursor-pointer hover:bg-stone-300 '
+                  >
+                    My Appointments
+                  </p>
+                  <p
+                    onClick={() => setToken(false)}
+                    className='p-3 hover:text-black cursor-pointer hover:bg-stone-300 hover:hover:rounded-b-lg'
+                  >
+                    Logout
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-        ) : (
-          <button
-            onClick={() => navigate('/login')}
-            className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'
-          >
-            Create Account
-          </button>
-        )}
-        <img
-          onClick={() => setShowMenu(true)}
-          className=' w-6 md:hidden'
-          src={assets.menu_icon}
-        />
-        {/*----------------- Mobile Menu ----------------------- */}
+          ) : (
+            <button
+              onClick={() => navigate('/login')}
+              className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'
+            >
+              Create Account
+            </button>
+          )}
+          <img
+            onClick={() => setShowMenu(true)}
+            className='w-6 lg:hidden cursor-pointer'
+            src={assets.menu_icon}
+            alt='Menu Icon'
+          />
+        </div>
+      </div>
+      {/* Mobile View */}
+      {showMenu && (
         <div
-          className={` ${
-            showMenu ? 'fixed w-full' : 'h-0 w-0'
-          } md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all duration-500`}
+          className={`${
+            showMenu ? 'fixed w-full h-full top-0  left-0' : 'h-0 w-0'
+          } bg-white transition-all duration-500 lg:hidden`}
         >
           <div className='flex items-center justify-between px-5 py-6'>
             <img
@@ -100,7 +106,7 @@ const Navbar = () => {
               alt=''
             />
             <img
-              className='w-7'
+              className='w-7 cursor-pointer'
               src={assets.cross_icon}
               onClick={() => setShowMenu(false)}
               alt=''
@@ -121,7 +127,7 @@ const Navbar = () => {
             </NavLink>
           </ul>
         </div>
-      </div>
+      )}
     </div>
   );
 };
